9a1b65fce6de112b575d010733c9c4e19e0978c3
Jonas228 <jonas-penner@t-online.de>
2017-11-28 12:32:51 +0100

    Variablen festlegen

---
+++ WindowsFormsApp1/CFahrzeug.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class CFahrzeug
+    {
+
+        //Free Game assets für Graphic nachschauen https://bayat.itch.io/platform-game-assets
+
+        protected double pGeschwindigkeit ; //protectetd  die vererbten können den Wert ändern
+        private double pBeschleunigung , pMaxGeschwindigkeit ;
+        private double pXStarposition, pYStartposition, plaenge;
+        protected Color farbe;
+        private List<CFahrzeug> kolonne; //List damit die Anzahl der Fahrzeuge nicht manuell eingegeben werden muss
+
+        public CFahrzeug(int Y, Color Farbe, int Laenge, double MaxGeschwindigkeit, double Beschleunigung)
+        {
+            if (kolonne == null)
+            {
+                kolonne = new List<CFahrzeug>();
+            }
+            pXStarposition = 100 - Laenge;
+            pYStartposition = Y;
+            farbe = Farbe;
+            plaenge = Laenge;
+            pGeschwindigkeit = 0;
+            pMaxGeschwindigkeit = MaxGeschwindigkeit;
+            pBeschleunigung = Beschleunigung;
+            foreach (CFahrzeug K in kolonne)
+            {
+             
+            }
+            kolonne.Add(this);
+        }
+
+
+
+
+    }
+}
--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 422);
+            this.ClientSize = new System.Drawing.Size(824, 605);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
